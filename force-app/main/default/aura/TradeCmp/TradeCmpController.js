6f4a6e13c4f7dd1b24e8c15348762dd8f8851466
Thiagoluizstr <thiagoluizstr@hotmail.com>
2021-09-13 20:21:54 -0300

    Atualização

--- force-app/main/default/aura/TradeCmp/TradeCmpController.js
+++ force-app/main/default/aura/TradeCmp/TradeCmpController.js
@@ -1,10 +1,73 @@
 ({
-    handleOrder : function(component, event, helper) {
-
+    retrievePrice : function(component, event, helper) {
+        helper.retrievePrice(component, event, helper);
     },
 
-    handledChange : function(component, event, helper){
-        //console.log(event.getParam("checked")[0]);
+    handleChange : function(component, event, helper) {
+        //console.log(event.getParam("checked"));
         component.set("v.opMercado", !event.getParam("checked"));
+        helper.retrievePrice(component, event, helper);
+    },
+
+    handleSuccess : function(component, event, helper){
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title" : 'Sucesso!',
+            "message" : 'Registro inserido!',
+            "type" : "success",
+            "duration" : 3000
+        });
+        toastEvent.fire();
+
+        component.find("ordField").forEach(function(f){
+            f.reset();
+        });
+    },
+
+    handleOrder : function(component, event, helper) {
+        event.preventDefault();
+        var fields = event.getParam("fields");
+        
+        var action = component.get("c.getRecordTypeId");
+        action.setParams({
+            "orderType" : "Compra"
+        });
+
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if(state == "SUCCESS"){
+               var rtId = response.getReturnValue();
+               fields["RecordTypeId"] = rtId;
+               if(fields["OperacaoMercado__c"] == false){
+                    fields["Status__c"] = "Aguardando";
+               }else{
+                    fields["Status__c"] = "Executada";
+               }
+               if(fields["Preco__c"] == undefined){
+                    fields["Preco__c"] = component.get("v.currentPrice");
+               }
+               component.find('orderForm').submit(fields);
+            }else{
+                console.log("Erro: " + JSON.stringify(response.getError()));
+            }
+        });
+
+        $A.enqueueAction(action);
+    },
+
+    handleQuantityPriceChange : function(component, event, helper) {
+        var price = component.get("v.price");
+        var quantity = component.get("v.quantity");
+        var total = price*quantity;
+
+        component.set("v.total", total);
+    },
+
+    handleTotalChange : function(component, event, helper) {
+        var price = component.get("v.price");
+        var total = component.get("v.total");
+        var quantity = total/price;
+
+        component.set("v.quantity", quantity);
     }
 })
---
+++ force-app/main/default/lwc/assetPainel/assetPainel.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class AssetPainel extends LightningElement {}
